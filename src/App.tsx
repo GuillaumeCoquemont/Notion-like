73c1eb5da515f462876be233d0a82e98dfd703be
Guillaume Coquemont <gui.coquemont@gmail.com>
2026-03-25 17:29:37 +0100

    init: project setup with vite + react + tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+    return (
+      <div className="min-h-screen bg-neutral-100 flex items-center justify-center">
+        <h1 className="text-3xl font-bold">Notion Like</h1>
+      </div>
+    );
+  }
+
+export default App
